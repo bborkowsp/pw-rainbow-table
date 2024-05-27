130c64971d72f0248d34bab424ea42b697ac2e63
bborkowsp <pawel.borkowski0704@gmail.com>
2024-05-27 16:01:07 +0200

    Reduction function class update

--- prir-project/src/main/java/org/example/ReductionFunction.java
+++ prir-project/src/main/java/org/example/ReductionFunction.java
@@ -13,12 +13,20 @@ public class ReductionFunction {
         StringBuilder reducedKey = new StringBuilder(KEY_LENGTH);
 
         for (int i = 0; i < KEY_LENGTH; i++) {
-            int hashIndex = (index + i) % hash.length();
+            int hashIndex = getHashIndex(hash.length(), index, i);
             char hashChar = hash.charAt(hashIndex);
-            int charsetIndex = (CHARSET.indexOf(hashChar) + index + i) % CHARSET.length();
+            int charsetIndex = getCharsetIndex(hashChar, index, i);
             reducedKey.append(CHARSET.charAt(charsetIndex));
         }
 
         return reducedKey.toString();
+    }
+
+    private int getCharsetIndex(char hashChar, int index, int i) {
+        return (CHARSET.indexOf(hashChar) + index + i) % CHARSET.length();
+    }
+
+    private int getHashIndex(int hashLength, int index, int i) {
+        return (index + i) % hashLength;
     }
 }
