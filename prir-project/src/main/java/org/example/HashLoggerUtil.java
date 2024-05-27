ff350be82038fc3a6897f97323ccd0ccb0c00790
bborkowsp <pawel.borkowski0704@gmail.com>
2024-05-27 15:56:55 +0200

    safe delete unused function

--- prir-project/src/main/java/org/example/HashLoggerUtil.java
+++ prir-project/src/main/java/org/example/HashLoggerUtil.java
@@ -6,11 +6,6 @@ public class HashLoggerUtil {
 
     private static final HashMap<String, String> generatedSubstitutes = new HashMap<>();
 
-    public static String getHashSubstitute(byte[] hash) {
-        if (hash == null) return null;
-        return getHashSubstitute(new String(hash));
-    }
-
     public static String getHashSubstitute(String hash) {
         if (generatedSubstitutes.containsKey(hash)) {
             return generatedSubstitutes.get(hash);
